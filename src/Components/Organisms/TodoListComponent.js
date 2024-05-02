ad27013d9d7f74220fb1bf1c3bde881d619c5764
EmadNejati <150430677+EmadNejati@users.noreply.github.com>
2024-05-02 14:10:56 +0330

    Add a new item to top of the todo list

--- src/Components/Organisms/TodoListComponent.js
+++ src/Components/Organisms/TodoListComponent.js
@@ -9,16 +9,36 @@ function TodoListComponent() {
   let [newTitle, setNewTitle] = useState("");
   let [newDate, setNewDate] = useState("");
   let [taskList, setTaskList] = useState([
-    { title: "static test 1", text: "this static sample number 1", date: "2024/02/16" },
-    { title: "static test 2", text: "this static sample number 2", date: "2025/05/26" },
-    { title: "static test 3", text: "this static sample number 3", date: "2023/02/10" },
-    { title: "static test 4", text: "this static sample number 4", date: "2020/11/02" },
-    { title: "static test 5", text: "this static sample number 5", date: "2021/07/16" }
+    {
+      title: "static test 1",
+      text: "this static sample number 1",
+      date: "2024/02/16",
+    },
+    {
+      title: "static test 2",
+      text: "this static sample number 2",
+      date: "2025/05/26",
+    },
+    {
+      title: "static test 3",
+      text: "this static sample number 3",
+      date: "2023/02/10",
+    },
+    {
+      title: "static test 4",
+      text: "this static sample number 4",
+      date: "2020/11/02",
+    },
+    {
+      title: "static test 5",
+      text: "this static sample number 5",
+      date: "2021/07/16",
+    },
   ]);
   const submitNewTask = () => {
     const newTaskList = [
-      ...taskList,
       { title: newTitle, text: newTask, date: newDate },
+      ...taskList,
     ];
     if (newTitle && newTask && newDate) {
       setTaskList(newTaskList);
